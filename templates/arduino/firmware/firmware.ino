c1848e74334a985f19ffd1463dbc55c7c2ffbacf
Matthew Gates <matthewg42@gmail.com>
2017-05-17 11:16:41 +0100

    flesh out template project

--- templates/arduino/firmware/firmware.ino
+++ templates/arduino/firmware/firmware.ino
@@ -1,17 +1,16 @@
 #include <Arduino.h>
-#include <Heartbeat.h>
-
-Heartbeat heartbeat(13);
+#include <MutilaDebug.h>
+#include "HeartBeat.h"
 
 void setup()
 {
     Serial.begin(115200);
-    heartbeat.begin();
-    Serial.println(F("setup:E"));
+    HeartBeat.begin();
+    DBLN(F("E:setup"));
 }
 
 void loop()
 {
-    heartbeat.update();
+    HeartBeat.update();
 }
 
