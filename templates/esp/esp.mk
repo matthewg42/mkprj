0f2cf821a476d0e1bb53f3f1d5d367177b989101
Matthew Gates <matthewg42@gmail.com>
2017-06-10 16:02:41 +0100

    merge esp8266 and esp32

--- templates/esp/esp.mk
+++ templates/esp/esp.mk
@@ -4,6 +4,12 @@ ifndef ESP_MAKEFILE
 	ESP_MAKEFILE := /opt/makeEspArduino/makeEspArduino.mk
 endif
 
+ifeq ($(CHIP), esp32)
+	ARDUINO_ROOT := $(shell grep -o "sketchbook.path=.*" $(HOME)/.arduino/preferences.txt 2>/dev/null | cut -f2- -d=)
+	ARDUINO_LIBS := $(ARDUINO_ROOT)/libraries
+	ESP_ROOT     := $(ARDUINO_ROOT)/hardware/espressif/esp32
+endif
+
 include $(ESP_MAKEFILE)
 
 
