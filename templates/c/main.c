3588a9e2039fd6926c499227d43092156416f12d
Matthew Gates <matthewg42@gmail.com>
2021-07-31 11:57:17 +0100

    add simdb to c project

--- templates/c/main.c
+++ templates/c/main.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
-
+// (C) __YEAR__ __AUTHOR__
 //
 // __SHORT_DESC__
 //
 
+#include <stdio.h>
+#include <simdb.h>
+
 int main(int argc, char** argv)
 {
-    printf("hi\n");
+    db_init(stderr, argv[0], db_i, 0);
+    db(db_i, "The program was invoked with %d command line arguments,\n", argc-1);
     return 0;
 }
 
